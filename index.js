518f1b7851c08e3a11409979d61c3b795788036a
bwrosen20 <bwrosen20@gmail.com>
2022-12-12 16:36:00 -0500

    commit tree 568c2fe34e919477ed10566f02726f48adf626e3

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1=31;
+const num2=2;
+var multiply = num1*num2;
 
-// Write your code here
+var random=Math.floor(Math.random() *10) + 1;
+
+const num3=28;
+const num4=6;
+let mod= num3 % num4;
+
+let max=20;
